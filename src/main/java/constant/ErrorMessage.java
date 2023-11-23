855f798b0abacccdeab7598ef0e5dc91cfd522ea
KimDongEon <gold3987@naver.com>
2023-11-23 15:19:31 +0900

    feat(ErrorMessage) : 에러 메세지 상수 구현

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,4 +1,20 @@
 package constant;
 
 public enum ErrorMessage {
+
+  NUMBER("금액은 숫자여야 합니다."),
+  DIVISIBLE_BY_TEN("10의 단위로 떨어져야 합니다."),
+  PRODUCT_FORM("[상품명,가격,수량];[상품명,가격,수량] 형식으로 입력해야 합니다."),
+  PRODUCT_NONE("없는 상품명 입니다.");
+
+
+  private final String message;
+
+  ErrorMessage(String message) {
+    this.message = "[Error] " + message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
