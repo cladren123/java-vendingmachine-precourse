2888fc8e6a453deb0dff678dc138dc4bd04b07e9
KimDongEon <gold3987@naver.com>
2023-11-23 15:45:03 +0900

    feat(Coin) : 금액이 들어오면 최소 코인 금액 반환 구현

--- src/main/java/constant/Coin.java
+++ src/main/java/constant/Coin.java
@@ -1,5 +1,8 @@
 package constant;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -13,4 +16,18 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+
+    // 금액이 들어오면 최소 동전 개수 반환
+    public static Map<Coin, Integer> calculateCoint(int money) {
+        Map<Coin, Integer> result = new LinkedHashMap<>();
+
+        for(Coin coin : Coin.values()) {
+            int count = money / coin.amount;
+            money -= money * coin.amount;
+            result.put(coin, count);
+        }
+
+        return result;
+    }
+
 }
