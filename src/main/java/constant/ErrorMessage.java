acf7e1544ff95c40574dfc8f7fcd63dcf4305e52
KimDongEon <gold3987@naver.com>
2023-11-25 18:49:04 +0900

    feat(ErrorMessage) : 양수 에러 메세지 구현

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -3,6 +3,7 @@ package constant;
 public enum ErrorMessage {
 
   NUMBER("금액은 숫자여야 합니다."),
+  POSITIVE_NUMBER("금액은 양수여야 합니다."),
   DIVISIBLE_BY_TEN("10의 단위로 떨어져야 합니다."),
   PRODUCT_FORM("[상품명,가격,수량];[상품명,가격,수량] 형식으로 입력해야 합니다."),
   PRODUCT_NONE("없는 상품명 입니다.");
