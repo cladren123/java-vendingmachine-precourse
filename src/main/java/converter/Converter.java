a194f08ca80618bf01cbfdd32985467425ac20b2
KimDongEon <gold3987@naver.com>
2023-11-25 23:31:01 +0900

    refactor : 금액 입력 검증 위치 변경

--- src/main/java/converter/Converter.java
+++ src/main/java/converter/Converter.java
@@ -4,12 +4,15 @@ import validate.Validate;
 
 public class Converter {
 
+
+
   // input을 money에 맞는지 변환
   public static Integer inputToMoney(String input) {
-    Validate.validateNumber(input);
-    int money = Integer.parseInt(input);
-    Validate.validatePositiveNumber(money);
-    Validate.validateDivisibleByTen(money);
-    return money;
+    return Integer.parseInt(input);
   }
+
+  // input을 상품명, 가격, 수량으로 구분
+
+
+
 }
--- src/main/java/service/VendingMachineService.java
+++ src/main/java/service/VendingMachineService.java
@@ -3,6 +3,7 @@ package service;
 import constant.Coin;
 import converter.Converter;
 import model.VendingMachineCoin;
+import validate.Validate;
 
 import java.util.Map;
 
@@ -14,7 +15,10 @@ public class VendingMachineService {
 
   // 금액을 입력하면 자판기가 보유한 동전으로 변환
   public static VendingMachineCoin inputVendingMachineMoney(String input) {
+    Validate.validateNumber(input);
     Integer money = Converter.inputToMoney(input);
+    Validate.validatePositiveNumber(money);
+    Validate.validateDivisibleByTen(money);
     Map<Coin, Integer> coins = Coin.calculateCoint(money);
     return new VendingMachineCoin(coins);
   }
