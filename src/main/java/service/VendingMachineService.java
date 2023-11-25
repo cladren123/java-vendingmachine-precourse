b078e9c8f29ed5bf2fb06de8e9106180d464a575
KimDongEon <gold3987@naver.com>
2023-11-25 18:36:20 +0900

    fix(VendingMachineService) : 상태 제거

--- src/main/java/service/VendingMachineService.java
+++ src/main/java/service/VendingMachineService.java
@@ -11,20 +11,17 @@ import java.util.Map;
  */
 public class VendingMachineService {
 
-  private final VendingMachineCoin vendingMachineCoin;
 
+  // 금액을 입력하면 자판기가 보유한 동전으로 변환
   public static VendingMachineCoin inputVendingMachineMoney(String input) {
     Integer money = Converter.StringToNumber(input);
     Map<Coin, Integer> coins = Coin.calculateCoint(money);
     return new VendingMachineCoin(coins);
   }
 
-  public VendingMachineService(VendingMachineCoin vendingMachineCoin) {
-    this.vendingMachineCoin = vendingMachineCoin;
-  }
 
   // 현재 보유한 동전 반환
-  public Map<Coin, Integer> getVendingMachineCoins() {
+  public Map<Coin, Integer> getVendingMachineCoins(VendingMachineCoin vendingMachineCoin) {
     return vendingMachineCoin.getCoins();
   }
 
