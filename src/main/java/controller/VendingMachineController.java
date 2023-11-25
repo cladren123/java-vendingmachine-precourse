1a4502c16e01dd99827e13abd317ff5377229dad
KimDongEon <gold3987@naver.com>
2023-11-25 18:33:49 +0900

    fix : 서비스 통합

--- src/main/java/controller/VendingMachineController.java
+++ src/main/java/controller/VendingMachineController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import model.VendingMachineCoin;
-import service.VendingMachineMoneyService;
 import service.VendingMachineService;
 import view.InputView;
 
@@ -9,14 +8,15 @@ public class VendingMachineController {
 
   // 프로그램 시작
   public static void start() {
-    inputVendingMachineMoney();
+    VendingMachineCoin vendingMachineCoin = inputVendingMachineMoney();
+    VendingMachineService vendingMachineService = new VendingMachineService(vendingMachineCoin);
   }
 
 
   // 자판기 보유 금액
   public static VendingMachineCoin inputVendingMachineMoney() {
     String input = InputView.inputVendingMachineMoney();
-    return VendingMachineMoneyService.inputVendingMachineMoney(input);
+    return VendingMachineService.inputVendingMachineMoney(input);
   }
 
 }
--- src/main/java/service/VendingMachineService.java
+++ src/main/java/service/VendingMachineService.java
@@ -1,6 +1,7 @@
 package service;
 
 import constant.Coin;
+import converter.Converter;
 import model.VendingMachineCoin;
 
 import java.util.Map;
@@ -11,6 +12,12 @@ import java.util.Map;
 public class VendingMachineService {
 
   private final VendingMachineCoin vendingMachineCoin;
+
+  public static VendingMachineCoin inputVendingMachineMoney(String input) {
+    Integer money = Converter.StringToNumber(input);
+    Map<Coin, Integer> coins = Coin.calculateCoint(money);
+    return new VendingMachineCoin(coins);
+  }
 
   public VendingMachineService(VendingMachineCoin vendingMachineCoin) {
     this.vendingMachineCoin = vendingMachineCoin;
