f67e3317675519e70b45b4189337ca591ac57980
KimDongEon <gold3987@naver.com>
2023-11-25 21:10:10 +0900

    fix(Application) : 생성자로 변경

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -5,6 +5,7 @@ import controller.VendingMachineController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        VendingMachineController.start();
+        VendingMachineController vendingMachineController = new VendingMachineController();
+        vendingMachineController.start();
     }
 }
